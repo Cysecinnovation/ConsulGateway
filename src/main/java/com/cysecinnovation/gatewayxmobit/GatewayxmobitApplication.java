0c7e18abf38b3d780309735de929775bfc924836
Martin Wambugu <martinleontech23@gmail.com>
2025-03-25 14:01:05 +0300

    ---update gateway

---
+++ src/main/java/com/cysecinnovation/gatewayxmobit/GatewayxmobitApplication.java
@@ -0,0 +1,13 @@
+package com.cysecinnovation.gatewayxmobit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayxmobitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayxmobitApplication.class, args);
+	}
+
+}
